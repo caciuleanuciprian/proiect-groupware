50c4f77ca29d9f7e93cc4d8510a462acde008df2
LeicaSerban <leicaserban@gmail.com>
2021-11-06 19:24:30 +0200

    new register

--- server/src/modules/users.js
+++ server/src/modules/users.js
@@ -3,19 +3,66 @@ const mongoose = require('mongoose');
 const usersSchema = new mongoose.Schema({
     username:{
         type: String,
-        unique: true
-
+        unique: true,
+        
     },
     password:{
-        type: String
+        type: String,
+        
     },
-    deletedAt: {
+
+    birthdate:{
         type: Date,
+        
+    },
+
+    country:{
+        type: String,
+        
+    },
+
+    city:{
+        type: String,
+        
+    },
+
+    secretQuestion: {
+        type: String,
+        
+    },
+
+    gender:{
+        type: String,
+        enum: ['male', 'female'],
+        
+    },
+
+    hobbies:{
+        type: String,
         default: null
+    },
+
+    friends:{
+        type: String,
+        default: null
+    },
+
+    groups:{
+        type: String,
+        default: null
+    },
+
+    interests:{
+        type: String,
+        default: null
+    },
+
+    deletedAt:{
+        type: Date,
+        default: null,
+        timestamps: true
     }
-}, {
-    timestamps: true,
-});
+    });
 
 const UsersModule = mongoose.model('users', usersSchema);
 module.exports = UsersModule 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -9,14 +9,19 @@ const route = express.Router();
 
 const userSchema = Joi.object({
     username: Joi.string().required(),
-    password: Joi.string().required()
+    password: Joi.string().required(),
+    birthdate: Joi.string().required(),
+    country: Joi.string().required(),
+    city: Joi.string().required(),
+    secretQuestion: Joi.string().required(),
+    gender: Joi.string().required(),
 })
 
 route.post('/create', async (req, res) =>{ 
     try{
-        const { username,password }  =  await userSchema.validateAsync(req.body)
+        const { username,password,birthdate, country,city,secretQuestion,gender }  =  await userSchema.validateAsync(req.body)
             .catch(() => { throw new Error({status: 400, message: 'Invalid Data'})});  
-        const data = await UsersModule.create([{ username, password}]);
+        const data = await UsersModule.create([{ username, password,birthdate, country,city,secretQuestion,gender}]);
         res.json(data[0]);
     }catch({status, message}){
         console.log(`ERROR status: ${status} message: ${message}`);
@@ -41,15 +46,29 @@ route.post('/login', async (req, res) =>{
         res.status(status || 500).json({message});
     }
 });
+
 route.get('/app', authMiddleware(async (req, res) => {
     
     console.log(req.headers.userId);
     res.json({});
-}))
+}));
 
+const getByIdSchema = Joi.object({
+    userId: Joi.string().required()
+});
 
+route.get('/:userId', async (req, res) => {
+    const { userId } = await getByIdSchema.validateAsync(req.params);
+    const user = await UsersModule.findById(userId);
+    res.json(user.deletedAt ? null : user);
+});
 
+route.delete('/delete/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const deletedUser = await UsersModule.findByIdAndUpdate(userId, { deletedAt: new Date() });
+    res.json(deletedUser);
 
+});
 
 
 module.exports = route;
