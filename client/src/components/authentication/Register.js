d5ce2e999d3920fc1161022cda1a8bf2241349a0
Ciprian <caciuleanu.ciprian@gmail.com>
2021-11-08 20:11:56 +0200

    finished register page

--- client/src/components/authentication/Register.js
+++ client/src/components/authentication/Register.js
@@ -1,16 +1,33 @@
 import React, { useState, Fragment, useEffect } from "react";
 import axios from "axios";
-import { Button, Grid, TextField, Container } from "@mui/material";
+import { MenuItem, Button, Grid, TextField, Container } from "@mui/material";
+import { countries } from "./countries";
+import { genders } from "./genders";
+
+// Add notification that the account was created!
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [user, setUser] = useState({ username, password });
+  const [birthdate, setBirthdate] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [secretQuestion, setSecretQuestion] = useState("");
+  const [gender, setGender] = useState("");
+  const [user, setUser] = useState({
+    username: username,
+    password: password,
+    birthdate: birthdate,
+    country: country,
+    city: city,
+    secretQuestion: secretQuestion,
+    gender: gender,
+  });
 
   useEffect(() => {
     handleUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [username, password]);
+  }, [username, password, birthdate, country, city, secretQuestion, gender]);
 
   const handleUsername = (event) => {
     setUsername(event.target.value);
@@ -20,14 +37,49 @@ const Register = () => {
     setPassword(event.target.value);
   };
 
+  const handleBirthdate = (event) => {
+    // const date = new Date(event.target.value).toISOString();
+    setBirthdate(event.target.value);
+  };
+
+  const handleCountry = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const handleCity = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSecretQuestion = (event) => {
+    setSecretQuestion(event.target.value);
+  };
+
+  const handleGender = (event) => {
+    setGender(event.target.value);
+  };
+
   const handleUser = () => {
-    setUser({ username: username, password: password });
+    setUser({
+      username: username,
+      password: password,
+      birthdate: birthdate,
+      country: country,
+      city: city,
+      secretQuestion: secretQuestion,
+      gender: gender,
+    });
   };
 
   const createAccount = () => {
     axios.post("http://localhost:8080/users/create", user).then((res) => {
+      console.log(user);
       setUsername("");
       setPassword("");
+      setBirthdate("");
+      setCountry("");
+      setCity("");
+      setSecretQuestion("");
+      setGender("");
       //redirect after register
     });
   };
@@ -56,6 +108,74 @@ const Register = () => {
             fullWidth
             type="password"
           />
+          <br />
+          <TextField
+            id="birthdate"
+            label=""
+            variant="outlined"
+            margin="dense"
+            onInput={handleBirthdate}
+            value={birthdate}
+            fullWidth
+            type="date"
+            helperText="Please enter your birthday."
+          />
+          <br />
+          <TextField
+            id="country"
+            select
+            margin="dense"
+            label="Country"
+            value={country}
+            onChange={handleCountry}
+            helperText="Please select your country."
+            fullWidth
+          >
+            {countries.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+          <br />
+          <TextField
+            id="city"
+            label="City"
+            variant="outlined"
+            margin="dense"
+            onInput={handleCity}
+            value={city}
+            fullWidth
+            type="text"
+          />
+          <br />
+          <TextField
+            id="secretQuestion"
+            label="Secret question"
+            variant="outlined"
+            margin="dense"
+            onInput={handleSecretQuestion}
+            value={secretQuestion}
+            fullWidth
+            type="text"
+          />
+          <br />
+          <TextField
+            id="gender"
+            select
+            margin="dense"
+            label="Gender"
+            value={gender}
+            onChange={handleGender}
+            helperText="Please select your gender."
+            fullWidth
+          >
+            {genders.map((gen) => (
+              <MenuItem key={gen} value={gen}>
+                {gen}
+              </MenuItem>
+            ))}
+          </TextField>
           <br />
           <Grid justifyContent="space-between" container sx={{ mt: 1, mb: 3 }}>
             <Button
--- server/src/modules/users.js
+++ server/src/modules/users.js
@@ -1,74 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        unique: true,
-        
-    },
-    password:{
-        type: String,
-        
-    },
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
 
-    birthdate:{
-        type: Date,
-        
-    },
+  birthdate: {
+    type: Date.UTC(),
+  },
 
-    country:{
-        type: String,
-        
-    },
+  country: {
+    type: String,
+  },
 
-    city:{
-        type: String,
-        
-    },
+  city: {
+    type: String,
+  },
 
-    secretQuestion: {
-        type: String,
-        
-    },
+  secretQuestion: {
+    type: String,
+  },
 
-    gender:{
-        type: String,
-        enum: ['male', 'female'],
-        
-    },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+  },
 
-    hobbies:{
-        type: String,
-        default: null
-    },
+  hobbies: {
+    type: String,
+    default: null,
+  },
 
-    friends:{
-        type: String,
-        default: null
-    },
+  friends: {
+    type: String,
+    default: null,
+  },
 
-    groups:{
-        type: String,
-        default: null
-    },
+  groups: {
+    type: String,
+    default: null,
+  },
 
-    interests:{
-        type: String,
-        default: null
-    },
+  interests: {
+    type: String,
+    default: null,
+  },
 
-    img:
-    {
-        data: Buffer,
-        contentType: String
-    },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 
-    deletedAt:{
-        type: Date,
-        default: null,
-        timestamps: true
-    }
-    });
+  deletedAt: {
+    type: Date,
+    default: null,
+    timestamps: true,
+  },
+});
 
-const UsersModule = mongoose.model('users', usersSchema);
-module.exports = UsersModule 
+const UsersModule = mongoose.model("users", usersSchema);
+module.exports = UsersModule;
