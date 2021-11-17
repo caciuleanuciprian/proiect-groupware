0bb6972e23c047b0aae10b8cc1b6ca3c60fcc943
LeicaSerban <leicaserban@gmail.com>
2021-11-17 10:37:00 +0200

    double /create deleted

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -41,28 +41,6 @@ route.post("/create", async (req, res) => {
   }
 });
 
-route.post("/create", async (req, res) => {
-  try {
-    const {
-      username,
-      password,
-      birthdate,
-      country,
-      city,
-      secretQuestion,
-      gender,
-    } = await userSchema.validateAsync(req.body).catch(() => {
-      throw new Error({ status: 400, message: "Invalid Data" });
-    });
-    const data = await UsersModule.create([
-      { username, password, birthdate, country, city, secretQuestion, gender },
-    ]);
-    res.json(data[0]);
-  } catch ({ status, message }) {
-    console.log(`ERROR status: ${status} message: ${message}`);
-    res.status(status || 500).json({ message });
-  }
-});
 
 route.post("/login", async (req, res) => {
   try {
