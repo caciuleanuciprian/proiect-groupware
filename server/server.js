9bb36c60755260eb53759cc0a265f76a3f183715
LeicaSerban <leicaserban@gmail.com>
2021-12-04 14:00:36 +0200

    incearca te rog Cip

--- server/server.js
+++ server/server.js
@@ -1,37 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const compression = require('compression');
+const express = require("express");
+const cors = require("cors");
+const compression = require("compression");
 const bodyParser = require('body-parser');
 
+const usersRoute = require("./src/routes/users");
+const imgRoute = require("./src/routes/img");
 
-
-const usersRoute = require('./src/routes/users');
-const imgRoute = require('./src/routes/img');
-
-const connectDB = require('./connection');
+const connectDB = require("./connection");
 connectDB();
 
 const server = express();
 server.use(cors(), compression(), express.json());
-server.use(express.urlencoded({extended: true})); 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 server.set("view engine", "ejs");
 
-server.use('/users', usersRoute);
-server.use('/img', imgRoute);
-
+server.use("/users", usersRoute);
+server.use("/img", imgRoute);
 
 const port = process.env.port || 8080;
 
-server.listen(port, function(error){
-    if(error){
-        console.log(`error opening server`)
-    }else
-        console.log(`Server has started on port: ${port}`);
+server.listen(port, function (error) {
+  if (error) {
+    console.log(`error opening server`);
+  } else console.log(`Server has started on port: ${port}`);
 });
-
-
-
-
-
-
