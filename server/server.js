694f4447ae4754a7c2ef87b577d61935f9eb9742
LeicaSerban <leicaserban@gmail.com>
2021-12-29 11:43:56 +0200

    socket

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ const bodyParser = require("body-parser");
 const usersRoute = require("./src/routes/users");
 const imgRoute = require("./src/routes/img");
 
+// import mongoDB connect
 const connectDB = require("./connection");
 connectDB();
 
+// create Express application
 const server = express();
+
 server.use(cors(), compression(), express.json());
 server.use(express.json({ limit: "50mb" }));
 server.use(
@@ -23,8 +26,32 @@ server.use("/img", imgRoute);
 
 const port = process.env.port || 8080;
 
+const io = require('socket.io')(5000);
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+    })
+    socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+    socket.emit('chat-message', { message: message, name: users[socket.id] });
+    })
+    socket.on('disconnect', name => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+    })
+})
+
+
+// Server has "started"
 server.listen(port, function (error) {
   if (error) {
     console.log(`error opening server`);
   } else console.log(`Server has started on port: ${port}`);
 });
+
+
+
